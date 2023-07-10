c7ac2bf07fcb3f03120cc3bd98cd0e6752ab9ff6
joker69216 <hamaraad883@gmail.com>
2023-07-10 18:19:45 +0300

    front-end\ starting with responsivity

--- client/app/components/Navbar/Navbar.tsx
+++ client/app/components/Navbar/Navbar.tsx
@@ -3,36 +3,41 @@ import './Navbar.scss';
 
 const Navbar = () => {
   return (
-    <nav className='desktop-nav'>
-      <div className='desktop-nav__left'>
-        <h1 className='desktop-nav__left__logo'>LOGO</h1>
-        <ul className='desktop-nav__left__list'>
-          {['Home', 'About', 'Trending', 'Contact', 'Blog'].map(
-            (item, index) => (
-              <li key={index} className='desktop-nav__left__list__items'>
-                <Link href={`/${item}`}>{item}</Link>
-              </li>
-            )
-          )}
-        </ul>
-      </div>
-      <div className='desktop-nav__right'>
-        <button type='button' className='desktop-nav__right__login'>
-          Login
-        </button>
-        <button type='button' className='desktop-nav__right__signup'>
-          Signup
-        </button>
-        <select
-          name='language'
-          title='langauge'
-          className='desktop-nav__right__language'
-        >
-          <option value='English'>EN</option>
-          <option value='Arabic'>AR</option>
-        </select>
-      </div>
-    </nav>
+    <>
+      <nav className='desktop-nav'>
+        <div className='desktop-nav__left'>
+          <h1 className='desktop-nav__left__logo'>LOGO</h1>
+          <ul className='desktop-nav__left__list'>
+            {['Home', 'About', 'Trending', 'Contact', 'Blog'].map(
+              (item, index) => (
+                <li key={index} className='desktop-nav__left__list__items'>
+                  <Link href={`/${item}`}>{item}</Link>
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+        <div className='desktop-nav__right'>
+          <button type='button' className='desktop-nav__right__login'>
+            Login
+          </button>
+          <button type='button' className='desktop-nav__right__signup'>
+            Signup
+          </button>
+          <select
+            name='language'
+            title='langauge'
+            className='desktop-nav__right__language'
+          >
+            <option value='English'>EN</option>
+            <option value='Arabic'>AR</option>
+          </select>
+        </div>
+      </nav>
+      <nav className='mobile-nav'>
+
+      </nav>
+    </>
   );
 };
 
