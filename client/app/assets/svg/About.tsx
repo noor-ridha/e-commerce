8c4e0f7bdbacc9ec2ba5130a44a798d6294b19cc
joker69216 <hamaraad883@gmail.com>
2023-07-11 19:08:05 +0300

    front-end\ add needed icons

---
+++ client/app/assets/svg/About.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="800"
+      height="800"
+      fill="#000"
+      data-name="Layer 1"
+      viewBox="0 0 52 52"
+    >
+      <path d="M26 52A26 26 0 0122.88.19 25.78 25.78 0 0134.73 1.5a2 2 0 11-1.35 3.77 22 22 0 00-21 38 22 22 0 0035.41-20 2 2 0 114-.48A26 26 0 0126 52z"></path>
+      <path d="M26 43.86a2 2 0 01-2-2v-19.2a2 2 0 114 0v19.2a2 2 0 01-2 2z"></path>
+      <circle cx="26" cy="15.71" r="2.57"></circle>
+    </svg>
+  );
+}
+
+export default About;
---
+++ client/app/assets/svg/Contact.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='800'
+      height='800'
+      viewBox='0 0 24 24'
+    >
+      <g fill='none' fillRule='evenodd' stroke='none' strokeWidth='1'>
+        <g transform='translate(-251 -207)'>
+          <g transform='translate(251 207)'>
+            <path
+              fill='#FFF'
+              fillOpacity='0.01'
+              fillRule='nonzero'
+              d='M0 0H24V24H0z'
+            ></path>
+            <path d='M18 16a4 4 0 100-8'></path>
+            <path
+              stroke='#212121'
+              strokeLinejoin='round'
+              strokeWidth='1.5'
+              d='M18 16h0a4 4 0 100-8'
+            ></path>
+            <path d='M6 8a4 4 0 100 8'></path>
+            <path
+              stroke='#212121'
+              strokeLinejoin='round'
+              strokeWidth='1.5'
+              d='M6 8a4 4 0 100 8h0'
+            ></path>
+            <path
+              stroke='#212121'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='1.5'
+              d='M6 16V8a6 6 0 0112 0v8a6 6 0 01-6 6'
+            ></path>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default Contact;
