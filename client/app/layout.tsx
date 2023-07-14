7e8f149379fab8d0a99f2d4fedcdbd3d5ed0dd02
joker69216 <hamaraad883@gmail.com>
2023-07-14 15:18:43 +0300

    add styles to the pages

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        {children}
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
