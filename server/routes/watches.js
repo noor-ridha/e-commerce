7390734997fb06be6ce1f23229f67b2c825cc88c
noor <noorridha028@gmail.com>
2023-07-15 12:59:47 +0300

    post req is done and we have our first object

--- server/routes/watches.js
+++ server/routes/watches.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const wateches = require("../ models/watchModel");
 // get all watches
 router.get("/", (req, res) => {
   res.json({ message: " get all watches route" });
@@ -10,8 +10,24 @@ router.get("/:id", (req, res) => {
   res.json({ message: "single watch " });
 });
 
-router.post("/", (req, res) => {
-  res.json({ message: "hello from post route" });
+// create a product
+router.post("/", async (req, res) => {
+  const { title, picture, price, gender, color, description, brand } = req.body;
+  try {
+    const watch = await wateches.create({
+      title,
+      picture,
+      price,
+      gender,
+      color,
+      description,
+      brand,
+    });
+    res.status(200).json(watch);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+  // res.json({ message: "hello from post route" });
 });
 router.patch("/:id", (req, res) => {
   res.json({ message: "edit" });
