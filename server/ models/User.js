7a11d9cf93424f3429f093ae284677b7614193c5
noor <noorridha028@gmail.com>
2023-07-20 20:40:33 +0300

    add users validators

--- server/ models/User.js
+++ server/ models/User.js
@@ -1,27 +1,29 @@
 const mongoose = require("mongoose");
-
+const validator = require("validator");
 const userSchema = mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, "Please enter your name"],
   },
 
   email: {
     type: String,
-    required: true,
+    required: [true, "Please enter your E-mail"],
     unique: true,
+    validate: [validator.isEmail, "enter correct E-mail"],
   },
   password: {
     type: String,
-    required: true,
+    required: [true, "Please enter your Password"],
     minLength: 8,
   },
   passwordConfirm: {
     type: String,
-    required: true,
+    required: [true, "Please Re-enter your Password"],
     unique: true,
   },
   photo: {
     type: String,
   },
 });
+module.exports = mongoose.model("users,userSchema ");
