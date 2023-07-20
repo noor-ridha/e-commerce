4d231ac46b126066228a790b68dfdaf3f46b726f
noor <noorridha028@gmail.com>
2023-07-20 12:45:19 +0300

    add models to authentication

--- server/ models/User.js
+++ server/ models/User.js
@@ -1,1 +1,27 @@
 const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  photo: {
+    type: String,
+  },
+});
