4ea3b9e0b8577219159113a139d4734c3202fd1d
joker69216 <hamaraad883@gmail.com>
2023-07-22 23:00:58 +0300

    front-end\ add basic item design style

--- client/app/home/Items/WatchesItem.tsx
+++ client/app/home/Items/WatchesItem.tsx
@@ -1,18 +1,22 @@
 import './WatchesItem.scss';
 
 const WatchesItem = () => {
-  return <div className="watch-item">
-    <div className="watch-item__image">
-      <img src="https://images.unsplash.com/photo-1612837017391-4b6b7b0b0b0b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2F0Y2hlc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80" alt="watch" />
+  return (
+    <div className="watch-item">
+      <img
+        src="https://i.ebayimg.com/images/g/anUAAOSwHUZjaT7N/s-l1600.jpg"
+        alt="watch"
+        className="watch-item__image"
+      />
+      <div className="watch-item__info">
+        <div className="watch-item__info__name">Watch Name</div>
+        <div className="watch-item__info__price">$100</div>
+        <button type="button" className="watch-item__button">
+          Discover
+        </button>
+      </div>
     </div>
-    <div className="watch-item__info">
-      <div className="watch-item__info__name">Watch Name</div>
-      <div className="watch-item__info__price">$100</div>
-    </div>
-    <button type='button' className="watch-item__button">
-      Discover
-    </button>
-  </div>;
+  );
 };
 
 export default WatchesItem;
