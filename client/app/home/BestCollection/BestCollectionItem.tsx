292a7d571aefc37159d9bbbbc13f15c9e61b9235
joker69216 <hamaraad883@gmail.com>
2023-07-23 15:32:48 +0300

    front-end\ add styles

--- client/app/home/BestCollection/BestCollectionItem.tsx
+++ client/app/home/BestCollection/BestCollectionItem.tsx
@@ -3,19 +3,21 @@ import './BestCollectionItem.scss';
 const BestCollectionItem = () => {
   return (
     <div className="collection-item">
-      <img
-        className="collection-item__image"
-        alt="watch"
-        src="https://i.ebayimg.com/images/g/anUAAOSwHUZjaT7N/s-l1600.jpg"
-      />
+      <div className="collection-item__img-bg">
+        <img
+          className="collection-item__img-bg__image"
+          alt="watch"
+          src="https://i.ebayimg.com/images/g/anUAAOSwHUZjaT7N/s-l1600.jpg"
+        />
+      </div>
       <div className="collection-item__info">
-        <h2 className="collection-item__info__title"></h2>
-        <p className="collection-item__info__price"></p>
+        <h2 className="collection-item__info__title">Rolex Purple Diamond</h2>
+        <p className="collection-item__info__price">6,899 $</p>
         <button
           type="button"
           className="btn btn--primary collection-item__add-to-cart"
         >
-          Add to cart
+          Discover Me
         </button>
       </div>
     </div>
