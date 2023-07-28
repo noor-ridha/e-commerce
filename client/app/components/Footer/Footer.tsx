04469b2991a7883c2b03e7696d76953518e78eae
joker69216 <hamaraad883@gmail.com>
2023-07-28 19:47:45 +0300

    front-end\ add styles to the quick links in footer

--- client/app/components/Footer/Footer.tsx
+++ client/app/components/Footer/Footer.tsx
@@ -13,9 +13,17 @@ const Footer = () => {
           />
           <h1 className="about-us__header__title">Company</h1>
         </div>
-        <p className="about-us__description">Best place for your time.</p>
+        <p className="about-us__description">Best place for your time</p>
       </div>
-      <div className="quick-links"></div>
+      <div className="quick-links">
+        <h1 className="quick-links__title">Quick Links</h1>
+        <ul className="quick-links__list">
+          <li className="quick-links__list__item">Home</li>
+          <li className="quick-links__list__item">About</li>
+          <li className="quick-links__list__item">Wish</li>
+          <li className="quick-links__list__item">Contact</li>
+        </ul>
+      </div>
       <div className="contact-us"></div>
     </footer>
   );
