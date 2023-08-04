7038394127388f1ac0ee2c4470a3fde2d3157662
unknown <hamaraad883@gmail.com>
2023-08-04 17:48:46 +0300

    front-end-home\ add hover effect to today item

--- client/app/home/TodayCollection/TodayCollectionItem.tsx
+++ client/app/home/TodayCollection/TodayCollectionItem.tsx
@@ -1,11 +1,29 @@
 import './TodayCollectionItem.scss';
 
+import { GrNext } from 'react-icons/gr';
+
 const TodayCollectionItem = () => {
   return (
-    <div className='today-item'>
-      <div className='today-item__circle-effect'/>
+    <div className="today-item">
+      <div className="today-item__circle-effect" />
+      <img
+        alt="img"
+        src="https://i.pinimg.com/originals/a9/35/20/a9352080b2127da1678cad38303a235e.png"
+        className="today-item__img"
+      />
+      <h2 className="today-item__title">Another Watch</h2>
+      <div className="today-item__info">
+        <p className="today-item__info__price">1000$</p>
+        <button
+          className="today-item__info__add-cart"
+          title="add"
+          type="button"
+        >
+          <GrNext className="today-item__info__add-cart__next" />
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default TodayCollectionItem;
