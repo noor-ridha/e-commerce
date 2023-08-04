9390c6500e6613d7addcd7a53febfaddb8c76ce7
unknown <hamaraad883@gmail.com>
2023-08-04 16:30:50 +0300

    front-end-home\ add structure for today item

---
+++ client/app/home/TodayCollection/TodayCollection.tsx
@@ -0,0 +1,12 @@
+import './TodayCollection.scss';
+
+const TodayCollection = () => {
+  return (
+    <div className='today-collection'>
+      <h2 className='today-collection__title'>Today's Collection</h2>
+      
+    </div>
+  )
+}
+
+export default TodayCollection;
---
+++ client/app/home/TodayCollection/TodayCollectionItem.tsx
@@ -0,0 +1,11 @@
+import './TodayCollectionItem.scss';
+
+const TodayCollectionItem = () => {
+  return (
+    <div className='today-item'>
+
+    </div>
+  )
+}
+
+export default TodayCollectionItem;
