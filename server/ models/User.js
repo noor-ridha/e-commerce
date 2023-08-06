453c40f629c438ee578b5d12acf6a6e98327b7c4
noor <noorridha028@gmail.com>
2023-08-06 19:22:38 +0300

    allow only the data need into the new user

--- server/ models/User.js
+++ server/ models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+
+// creating users
 const userSchema = mongoose.Schema({
   name: {
     type: String,
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,7 +2,13 @@ const Users = require("../ models/User");
 
 const signUp = async (req, res) => {
   try {
-    const newUser = await Users.create(req.body);
+    const newUser = await Users.create({
+      // here we only allowed the data that we need to be put into the new user
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+    });
 
     res.status(200).json(newUser);
   } catch (error) {
