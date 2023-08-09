91b1750a105cc06ae8a980aec6ecf99d1acaf6d2
mhamad raad <hamaraad883@gmail.com>
2023-08-09 03:18:45 +0300

    front-end\ add error handling

--- client/app/fetch/Watch.ts
+++ client/app/fetch/Watch.ts
@@ -1,7 +1,13 @@
 const getWatches: () => Promise<any[]> = async () => {
-  const res: Response = await fetch('http://localhost:7000/api/watches');
-  const watches: Array<any> = await res.json();
-  return watches;
+  try {
+    const res: Response = await fetch('http://localhost:7000/api/watches');
+    const watches: Array<any> = await res.json();
+    return watches;
+  }
+  catch (err) {
+    console.log(err);
+    return [];
+  }
 };
 
 export { getWatches };
