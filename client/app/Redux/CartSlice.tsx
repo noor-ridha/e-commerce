8cb4c8a6c8bd2f418611f9cf02629eb64836530c
mhamad raad <hamaraad883@gmail.com>
2023-08-10 20:13:32 +0300

    front-end\ update cart add function

--- client/app/Redux/CartSlice.tsx
+++ client/app/Redux/CartSlice.tsx
@@ -2,8 +2,9 @@
 
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState: { items: any[] } = {
+const initialState: { items: any[]; totalAmount: number } = {
   items: [],
+  totalAmount: 0,
 };
 
 export const cartSlice = createSlice({
@@ -12,7 +13,27 @@ export const cartSlice = createSlice({
   reducers: {
     addToCart: (state, action) => {
       const { payload } = action;
-      state.items = [...state.items, payload];
+      const updatedAmount = state.totalAmount + payload.price;
+      const existingItemIndex = state.items.findIndex(
+        (item) => item._id === payload._id,
+      ); 
+
+      const existingItem = state.items[existingItemIndex];
+      let updatedItems;
+      if (existingItem) {
+        if (typeof (existingItem.amount) !== 'number') existingItem.amount = 1;
+          const updatedItem = {
+            ...existingItem,
+            amount: existingItem.amount + 1,
+          };
+        updatedItems = [...state.items];
+        updatedItems[existingItemIndex] = updatedItem;
+        state.items = updatedItems;
+      } else {
+        state.items = [...state.items, {...payload, amount: 1}];
+      }
+      state.totalAmount = updatedAmount;
+      
     },
 
     removeFromCart: (state, action) => {
