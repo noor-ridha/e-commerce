2d967fecac126c8cb854782e57a35990a106330d
mhamad raad <hamaraad883@gmail.com>
2023-08-11 19:47:09 +0300

    front-end\ add content for the about us page

--- client/app/about/page.tsx
+++ client/app/about/page.tsx
@@ -1,9 +1,99 @@
 import './About.scss';
 
 const page = () => {
-  return <div className='about'>
-    <h2 className='about__title'>About us</h2>
-  </div>;
+  return (
+    <div className="about">
+      <h2 className="about__title">About us</h2>
+      <div className="about__row">
+        <img
+          alt="us"
+          className="about__row__img"
+          src="https://www.watchreviewblog.com/wp-content/uploads/2020/10/watch-brands-with-amazing-customer-service.jpeg"
+        />
+        <div className="about__row__info">
+          <h3 className="about__row__info__title">Our Short Story</h3>
+          <p className="about__row__info__text1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptatum, quibusdam, quia, quos voluptates voluptatem quod
+            voluptatibus quas doloribus quidem voluptate. Quisquam voluptatum,
+            quibusdam, quia, quos voluptates voluptatem quod voluptatibus quas
+            doloribus quidem voluptate.
+          </p>
+          <p className="about__row__info__text2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptatum, quibusdam, quia, quos voluptates voluptatem quod
+            voluptatibus quas doloribus quidem voluptate. Quisquam voluptatum.
+          </p>
+        </div>
+      </div>
+      <div className="about__row reverse-flex">
+        <img
+          alt="us"
+          className="about__row__img"
+          src="https://www.watchreviewblog.com/wp-content/uploads/2020/10/watch-brands-with-amazing-customer-service.jpeg"
+        />
+        <div className="about__row__info">
+          <h3 className="about__row__info__title">Our Short Story</h3>
+          <p className="about__row__info__text1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptatum, quibusdam, quia, quos voluptates voluptatem quod
+            voluptatibus quas doloribus quidem voluptate. Quisquam voluptatum,
+            quibusdam, quia, quos voluptates voluptatem quod voluptatibus quas
+            doloribus quidem voluptate.
+          </p>
+          <p className="about__row__info__text2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptatum, quibusdam, quia, quos voluptates voluptatem quod
+            voluptatibus quas doloribus quidem voluptate. Quisquam voluptatum.
+          </p>
+        </div>
+      </div>
+      <div className="about__row">
+        <img
+          alt="us"
+          className="about__row__img"
+          src="https://www.watchreviewblog.com/wp-content/uploads/2020/10/watch-brands-with-amazing-customer-service.jpeg"
+        />
+        <div className="about__row__info">
+          <h3 className="about__row__info__title">Our Short Story</h3>
+          <p className="about__row__info__text1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptatum, quibusdam, quia, quos voluptates voluptatem quod
+            voluptatibus quas doloribus quidem voluptate. Quisquam voluptatum,
+            quibusdam, quia, quos voluptates voluptatem quod voluptatibus quas
+            doloribus quidem voluptate.
+          </p>
+          <p className="about__row__info__text2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptatum, quibusdam, quia, quos voluptates voluptatem quod
+            voluptatibus quas doloribus quidem voluptate. Quisquam voluptatum.
+          </p>
+        </div>
+      </div>
+      <div className="about__row reverse-flex">
+        <img
+          alt="us"
+          className="about__row__img"
+          src="https://www.watchreviewblog.com/wp-content/uploads/2020/10/watch-brands-with-amazing-customer-service.jpeg"
+        />
+        <div className="about__row__info">
+          <h3 className="about__row__info__title">Our Short Story</h3>
+          <p className="about__row__info__text1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptatum, quibusdam, quia, quos voluptates voluptatem quod
+            voluptatibus quas doloribus quidem voluptate. Quisquam voluptatum,
+            quibusdam, quia, quos voluptates voluptatem quod voluptatibus quas
+            doloribus quidem voluptate.
+          </p>
+          <p className="about__row__info__text2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptatum, quibusdam, quia, quos voluptates voluptatem quod
+            voluptatibus quas doloribus quidem voluptate. Quisquam voluptatum.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
