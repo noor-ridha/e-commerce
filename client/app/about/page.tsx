8ea29c28e6e2acb06984f6208fbefb6b1a971076
mhamad raad <hamaraad883@gmail.com>
2023-08-12 18:14:47 +0300

    front-end\ add responsiveness to about page

--- client/app/about/page.tsx
+++ client/app/about/page.tsx
@@ -33,7 +33,7 @@ const page = () => {
           src="https://www.watchreviewblog.com/wp-content/uploads/2020/10/watch-brands-with-amazing-customer-service.jpeg"
         />
         <div className="about__row__info">
-          <h3 className="about__row__info__title">Our Short Story</h3>
+          <h3 className="about__row__info__title">Our Vision</h3>
           <p className="about__row__info__text1">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
             voluptatum, quibusdam, quia, quos voluptates voluptatem quod
@@ -55,29 +55,7 @@ const page = () => {
           src="https://www.watchreviewblog.com/wp-content/uploads/2020/10/watch-brands-with-amazing-customer-service.jpeg"
         />
         <div className="about__row__info">
-          <h3 className="about__row__info__title">Our Short Story</h3>
-          <p className="about__row__info__text1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            voluptatum, quibusdam, quia, quos voluptates voluptatem quod
-            voluptatibus quas doloribus quidem voluptate. Quisquam voluptatum,
-            quibusdam, quia, quos voluptates voluptatem quod voluptatibus quas
-            doloribus quidem voluptate.
-          </p>
-          <p className="about__row__info__text2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            voluptatum, quibusdam, quia, quos voluptates voluptatem quod
-            voluptatibus quas doloribus quidem voluptate. Quisquam voluptatum.
-          </p>
-        </div>
-      </div>
-      <div className="about__row reverse-flex">
-        <img
-          alt="us"
-          className="about__row__img"
-          src="https://www.watchreviewblog.com/wp-content/uploads/2020/10/watch-brands-with-amazing-customer-service.jpeg"
-        />
-        <div className="about__row__info">
-          <h3 className="about__row__info__title">Our Short Story</h3>
+          <h3 className="about__row__info__title">Our Approach</h3>
           <p className="about__row__info__text1">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
             voluptatum, quibusdam, quia, quos voluptates voluptatem quod
