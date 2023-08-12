c60435b4a51864ffd4b840ad8ff46603575ab8bc
mhamad raad <hamaraad883@gmail.com>
2023-08-12 18:21:08 +0300

    front-end\ add structure for the contact page

---
+++ client/app/contact/page.tsx
@@ -0,0 +1,17 @@
+import './Contact.scss';
+
+export default function page() {
+  return (
+    <div className="contact">
+      <h2 className="contact__title">Contact Us</h2>
+      <p className="contact__call-to-action">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quidem
+        nobis id unde error obcaecati aliquid dolores ipsa tempora amet cumque
+        alias, quaerat mollitia facere? Amet suscipit blanditiis dolorem maxime.
+      </p>
+      <form className='contact__form'>
+        
+      </form>
+    </div>
+  );
+}
